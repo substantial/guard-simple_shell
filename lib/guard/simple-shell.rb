8741222878c6494610340371b47506ce7b518d7d
Adam Pearson <adam@substantial.com>
2012-09-17 10:59:36 -0700

    initial commit

---
+++ lib/guard/simple-shell.rb
@@ -0,0 +1,9 @@
+require "guard-simple-shell/version"
+
+module Guard
+  module Simple
+    module Shell
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/guard/simple-shell/version.rb
@@ -0,0 +1,7 @@
+module Guard
+  module Simple
+    module Shell
+      VERSION = "0.0.1"
+    end
+  end
+end
